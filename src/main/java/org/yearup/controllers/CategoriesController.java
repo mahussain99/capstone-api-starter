9e54a4d12ecb9b3c3a4cf239bfd02ef619de4b4c
Family <mh5212357@gmail.com>
2025-12-18 09:28:22 -0500

    I organize my CategoriesController class

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -12,10 +12,6 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-// http://localhost:8080/categories
-// add annotation to allow cross site origin requests
 @RestController
 @CrossOrigin
 @RequestMapping("categories")
@@ -24,19 +20,15 @@ public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
     @Autowired
     public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
     }
 
-
-    // add the appropriate annotation for a get action
     @GetMapping("")
     @PreAuthorize("permitAll()")
     public List<Category> getAll() {
-        // find and return all categories
         try {
 
             return categoryDao.getAllCategories();
@@ -48,7 +40,6 @@ public class CategoriesController {
         }
     }
 
-    // add the appropriate annotation for a get action
     @GetMapping("{id}")
     @PreAuthorize("permitAll()")
     public Category getById(@PathVariable int id) {
@@ -58,7 +49,6 @@ public class CategoriesController {
             if (category == null)
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
-            // get the category by id
             return category;
 
         } catch (ResponseStatusException e) {
@@ -68,12 +58,10 @@ public class CategoriesController {
         }
     }
 
-    // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
     @PreAuthorize("permitAll()")
     public List<Product> getProductsById(@PathVariable int categoryId) {
-        // get a list of product by categoryId
 
         try {
             return productDao.listByCategoryId(categoryId);
@@ -83,13 +71,10 @@ public class CategoriesController {
 
     }
 
-    // add annotation to call this method for a POST action
-    // add annotation to ensure that only an ADMIN can call this function
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Category addCategory(@RequestBody Category category) {
-        // insert the category
         try {
             return categoryDao.create(category);
         } catch (Exception ex) {
@@ -97,13 +82,9 @@ public class CategoriesController {
         }
     }
 
-    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
     @PutMapping("{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category) {
-        // update the category by id
-
         try {
             categoryDao.update(id, category);
         } catch (Exception ex) {
@@ -111,14 +92,9 @@ public class CategoriesController {
         }
     }
 
-
-    // add annotation to call this method for a DELETE action - the url path must include the categoryId
-    // add annotation to ensure that only an ADMIN can call this function
-    @DeleteMapping("{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCategory(@PathVariable int id) {
-        // delete the category by id
         try {
             var category = categoryDao.getById(id);
 
